3c9bd62b25b03586d596d8c84857d28d117fb869
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2026-03-25 13:52:02 -0300

    Corrige interceptor para S3

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -5,8 +5,9 @@ import { JwtService } from '../services/jwt-service/jwt-service';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const jwtService = inject(JwtService);
     const token = jwtService.getToken();
+    const s3Url = 's3-s0c8c0880gso8s8gccco88ss.rotaslivres.com.br';
 
-    if (token) {
+    if (token && req.url.includes(s3Url)) {
         req = req.clone({
             setHeaders: {
                 Authorization: `Bearer ${token}`
