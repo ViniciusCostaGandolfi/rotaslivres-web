f255f929823ff0b50b65349f75d18ffe3db3f77e
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2026-03-25 13:54:03 -0300

    Corrige interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -7,7 +7,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const token = jwtService.getToken();
     const s3Url = 's3-s0c8c0880gso8s8gccco88ss.rotaslivres.com.br';
 
-    if (token && req.url.includes(s3Url)) {
+    if (token && !req.url.includes(s3Url)) {
         req = req.clone({
             setHeaders: {
                 Authorization: `Bearer ${token}`
