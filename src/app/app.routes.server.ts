37cdb60868f299b4d1817eccf43b69c7edef1182
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2026-04-22 21:29:47 -0300

    feat: add server-side rendering route for solution details by ID and index

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -10,6 +10,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client
   },
   {
+    path: ':solutionId/:routeIndex',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
